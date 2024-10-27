e7a33a2a617857f4acf78f9c9bdf4baa5368d47e
SaltyEightLab <hachiman_hachi@icloud.com>
2024-10-27 16:07:56 +0900

    section3.23まで

--- src/components/contact/Contact.test.tsx
+++ src/components/contact/Contact.test.tsx
@@ -2,9 +2,39 @@ import { render, screen } from "@testing-library/react";
 import Contact from "./Contact";
 
 describe("Contact", () => {
-  test("renders currectly", () => {
+  test("renders currectly textbox", () => {
     render(<Contact />);
-    const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", { name: "お名前" });
     expect(nameElement).toBeInTheDocument();
+  });
+
+  test("renders currectly name label", () => {
+    render(<Contact />);
+    const nameLabelElement = screen.getByLabelText("お名前");
+    expect(nameLabelElement).toBeInTheDocument();
+  });
+
+  test("renders currectly textbox", () => {
+    render(<Contact />);
+    const emailElement = screen.getByRole("textbox", { name: "メールアドレス" });
+    expect(emailElement).toBeInTheDocument();
+  });
+
+  test("renders currectly combobox", () => {
+    render(<Contact />);
+    const comboboxElement = screen.getByRole("combobox");
+    expect(comboboxElement).toBeInTheDocument();
+  });
+
+  test("renders currectly checkbox", () => {
+    render(<Contact />);
+    const checkboxElement = screen.getByRole("checkbox");
+    expect(checkboxElement).toBeInTheDocument();
+  });
+
+  test("renders currectly submit button", () => {
+    render(<Contact />);
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
   });
 });
