2775b8eb78755f59445e4204baca58144ed2f2c0
SaltyEightLab <hachiman_hachi@icloud.com>
2024-10-28 05:54:31 +0900

    section3.31

--- src/components/contact/Contact.test.tsx
+++ src/components/contact/Contact.test.tsx
@@ -2,6 +2,48 @@ import { render, screen } from "@testing-library/react";
 import Contact from "./Contact";
 
 describe("Contact", () => {
+  test("renders currectly h1text", () => {
+    render(<Contact />);
+    const h1Text = screen.getByRole("heading", { level: 1 });
+    expect(h1Text).toBeInTheDocument();
+  });
+
+  test("renders currectly h2text", () => {
+    render(<Contact />);
+    const h2Text = screen.getByRole("heading", { level: 2 });
+    expect(h2Text).toBeInTheDocument();
+  });
+
+  test("renders currectly span close", () => {
+    render(<Contact />);
+    const closeTitle = screen.getByTitle("close");
+    expect(closeTitle).toBeInTheDocument();
+  });
+
+  test("renders currectly paragraph", () => {
+    render(<Contact />);
+    const paragraphText = screen.getByText("すべてのフィールドは必須項目です。");
+    expect(paragraphText).toBeInTheDocument();
+  });
+
+  test("renders currectly image", () => {
+    render(<Contact />);
+    const imageElement = screen.getByAltText("sample alt");
+    expect(imageElement).toBeInTheDocument();
+  });
+
+  test("renders currectly custom element", () => {
+    render(<Contact />);
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+  });
+
+  test("renders currectly imageRle", () => {
+    render(<Contact />);
+    const imageRoleElement = screen.getByRole("img");
+    expect(imageRoleElement).toBeInTheDocument();
+  });
+
   test("renders currectly textbox", () => {
     render(<Contact />);
     const nameElement = screen.getByRole("textbox", { name: "お名前" });
@@ -12,6 +54,18 @@ describe("Contact", () => {
     render(<Contact />);
     const nameLabelElement = screen.getByLabelText("お名前");
     expect(nameLabelElement).toBeInTheDocument();
+  });
+
+  test("renders currectly name placeholder", () => {
+    render(<Contact />);
+    const namePlaceholderlElement = screen.getByPlaceholderText("フルネーム");
+    expect(namePlaceholderlElement).toBeInTheDocument();
+  });
+
+  test("renders currectly value SaltyEight", () => {
+    render(<Contact />);
+    const nameValueText = screen.getByDisplayValue("SaltyEight");
+    expect(nameValueText).toBeInTheDocument();
   });
 
   test("renders currectly textbox", () => {
